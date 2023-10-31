247b3032b002d8480d349bccf32d71b93f7261cc
IvanAndreevichPle <dr.plekhanov@gmail.com>
2023-10-31 15:55:19 +0300

    First handler

--- cmd/internal/user/handler.go
+++ cmd/internal/user/handler.go
@@ -21,33 +21,45 @@ func NewHandler() handlers.Handler {
 
 func (h *handler) Register(router *http.ServeMux) {
 	router.HandleFunc(usersURL, h.GetList)
-	router.HandleFunc(userURL, h.GetUserByUUID)
-	//router.HandleFunc(userURL, h.CreateUser)
-	//router.HandleFunc(userURL, h.UpdateUser)
-	//router.HandleFunc(userURL, h.DeleteUser)
 }
 
+func (h *handler) HandleUser(w http.ResponseWriter, r *http.Request) {
+	uuid := r.URL.Query().Get(":uuid")
+
+	switch r.Method {
+	case http.MethodGet:
+		h.GetUserByUUID(w, r, uuid)
+	case http.MethodPost:
+		h.CreateUser(w, r, uuid)
+	case http.MethodPut:
+		h.UpdateUser(w, r, uuid)
+	case http.MethodDelete:
+		h.DeleteUser(w, r, uuid)
+	default:
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+	}
+}
 func (h *handler) GetList(w http.ResponseWriter, r *http.Request) {
 	// Обработка GET-запроса для получения списка пользователей
 	w.Write([]byte("GetList: This is a list of users"))
 }
 
-func (h *handler) GetUserByUUID(w http.ResponseWriter, r *http.Request) {
+func (h *handler) GetUserByUUID(w http.ResponseWriter, r *http.Request, uuid string) {
 	// Обработка GET-запроса для получения пользователя по UUID
 	w.Write([]byte("GetUserByUUID: Getting user by UUID"))
 }
 
-func (h *handler) CreateUser(w http.ResponseWriter, r *http.Request) {
+func (h *handler) CreateUser(w http.ResponseWriter, r *http.Request, uuid string) {
 	// Обработка POST-запроса для создания пользователя
 	w.Write([]byte("CreateUser: Creating a new user"))
 }
 
-func (h *handler) UpdateUser(w http.ResponseWriter, r *http.Request) {
+func (h *handler) UpdateUser(w http.ResponseWriter, r *http.Request, uuid string) {
 	// Обработка PUT-запроса для обновления пользователя
 	w.Write([]byte("UpdateUser: Updating user"))
 }
 
-func (h *handler) DeleteUser(w http.ResponseWriter, r *http.Request) {
+func (h *handler) DeleteUser(w http.ResponseWriter, r *http.Request, uuid string) {
 	// Обработка DELETE-запроса для удаления пользователя
 	w.Write([]byte("DeleteUser: Deleting user"))
 }
